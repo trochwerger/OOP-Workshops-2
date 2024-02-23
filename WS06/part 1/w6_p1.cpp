83e75981085ec85a8c6b6864d83dc2fbed3c349e
Cornel Barna <cornel.barna@senecacollege.ca>
2024-02-23 14:29:58 -0500

    Workshop #6 has been released!

---
+++ WS06/part 1/w6_p1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Utilities.h"
+#include "Utilities.h"
+#include "College.h"
+#include "College.h"
+
+int cout = 0; // won't compile if headers don't follow convention regarding namespaces
+
+void loadData(const char* filename, seneca::College& theCollege)
+{
+	std::ifstream file(filename);
+	if (!file)
+	{
+		std::cerr << "ERROR: Cannot open file [" << filename << "].\n";
+		return;
+	}
+	seneca::Person* thePerson = nullptr;
+	while (file)
+	{
+		// TODO: This code can throw errors to signal that something 
+		//         went wrong while extracting data. Write code to catch
+		//         and handle the exceptions.
+		thePerson = seneca::buildInstance(file);
+		if (thePerson)
+			theCollege += thePerson;
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	std::cout << "Command Line:\n";
+	std::cout << "--------------------------\n";
+	for (int i = 0; i < argc; ++i)
+		std::cout << std::setw(3) << i + 1 << ": " << argv[i] << '\n';
+	std::cout << "--------------------------\n\n";
+
+	seneca::College theCollege;
+	::loadData(argv[1], theCollege);
+	theCollege.display(std::cout);
+
+	return cout;
+}
